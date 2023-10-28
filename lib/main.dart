2078375900b35dced2082c74443c5bb0586db984
SonamTenzin7 <02210223.cst@rub.edu.bt>
2023-10-28 19:58:03 +0600

    hhjhjhj

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,22 @@
 import 'package:flutter/material.dart';
-import 'hostel.dart';
-
-class HomePageWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1A08B7),
-        automaticallyImplyLeading: false,
-        actions: [],
-        centerTitle: false,
-        elevation: 2,
-      ),
-      body: SafeArea(
-        top: true,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center, // Center the children vertically.
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-              child: Container(
-                width: double.infinity,
-                height: 164,
-                child: Center(
-                  child: Card(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    color: Colors.blue,
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child:InkWell(
-                      onTap:() {
-                        Navigator.of(context).pushNamed('/hostels');
-                      },
-                      child: Center(
-                          child: Text(
-                            'Hostels',
-                            style: TextStyle(
-                              fontFamily: 'Readex Pro',
-                              fontSize: 40,
-                            ),
-                          ),
-                        ),
-                    )       
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-              child: Container(
-                width: double.infinity,
-                height: 164,
-                child: Center(
-                  child: Card(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    color: Colors.red,
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Maintenance',
-                        style: TextStyle(
-                          fontFamily: 'Readex Pro',
-                          fontSize: 40,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-              child: Container(
-                width: double.infinity,
-                height: 164,
-                child: Center(
-                  child: Card(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    color: Colors.green,
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontFamily: 'Readex Pro',
-                          fontSize: 40,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+import 'package:studentlogin/login.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePageWidget(),
-    routes:{
-      '/hostels':(context) => HostelsWidget(),
-    }
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hostel Management System',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF0028A8),
+      ),
+      home: Login(),
+    );
+  }
 }
